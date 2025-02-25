e4bfa419f5993f4f7905f209bbf5549b57ba7be4
Oscar Blanco <oscarmb86@hotmail.com>
2025-02-25 09:44:40 -0600

    adde user and password using .env

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,24 +1,47 @@
-// screens/LoginScreen.js
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Image, Alert } from 'react-native';
+import { VALID_EMAIL, VALID_PASSWORD } from '@env';
 
-import React from 'react';
-import { View, Text, Button, TextInput, StyleSheet, Image } from 'react-native';
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-const LoginScreen = ({ navigation }) => {
+  const handleLogin = () => {
+    if (email === VALID_EMAIL && password === VALID_PASSWORD) {
+      navigation.navigate('MainMenu'); // Navigate if correct
+    } else {
+      Alert.alert('Invalid Credentials', 'Please check your email and password.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Image source={require('./../assets/logo.png')} style={styles.logo}/>
-      <Text style={styles.title}>AgriNova Login</Text>
-      <TextInput style={styles.input} placeholder="Username" />
-      <TextInput style={styles.input} placeholder="Password" secureTextEntry />
-      <Button title="Login" onPress={() => navigation.navigate('Options')} />
+      <Image source={require('./../assets/logo.png')} style={styles.logo} />
+      <Text style={styles.title}>MindClusive</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Enter" onPress={handleLogin} />
+      <Text style={styles.link}>Forgot Password? Sign Up</Text>
     </View>
   );
-};
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center', // Added to center the image horizontally
+    alignItems: 'center', // Center the image horizontally
     padding: 16,
   },
   title: {
@@ -40,7 +63,9 @@ const styles = StyleSheet.create({
     height: 150,
     resizeMode: 'contain', // or 'cover' depending on your needs
     marginBottom: 24, // Adds space between the logo and the title
-  }
+  },
+  link: {
+    marginTop: 10,
+    color: 'blue',
+  },
 });
-
-export default LoginScreen
